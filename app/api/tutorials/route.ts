e4dcda1d1a21175e0a90ec7a6c24b8bcc1e6bbb9
BulhakovOzero <56506157+AndriiBulhakov@users.noreply.github.com>
2024-06-06 11:25:35 +0200

    change tutor collection

--- app/api/tutorials/route.ts
+++ app/api/tutorials/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
 
   try {
     const response = await axios.get(
-      "https://api.webflow.com/v2/collections/66504059267b8c912163c50d/items",
+      "https://api.webflow.com/v2/collections/66617f10c36b24ec40c12388/items",
       {
         headers: {
           Accept: "application/json",
